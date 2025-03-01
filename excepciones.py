2d5bdcab063dc580906ae59b2962a366e6fe562e
erickevsh <eric.flores.2024@cbtis-246.edu.mx>
2025-02-28 20:01:24 -0600

    Add files via upload

---
+++ excepciones.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+try:
+    
+
+
+# In[11]:
+
+
+my_list = [1, 2, 3]
+print(my_dict['b'])
+
+
+# In[13]:
+
+
+my_dict = {'a': 1}
+print(my_dict['b'])
+
+
+# In[15]:
+
+
+int('Hello')
+
+
+# In[17]:
+
+
+'1' + 1
+
+
+# In[19]:
+
+
+1 / 0
+
+
+# In[21]:
+
+
+open('nonexistent_file.txt')
+
+
+# In[23]:
+
+
+'hello'.non_existent_method()
+
+
+# In[25]:
+
+
+import nonexistent_module
+
+
+# In[29]:
+
+
+assert 1 == 2
+
+
+# In[ ]:
+
+
+
+
