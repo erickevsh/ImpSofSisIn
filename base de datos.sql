efbc528a265f6112b2913a488d15658713e18901
erickevsh <eric.flores.2024@cbtis-246.edu.mx>
2025-04-04 20:20:47 -0600

    Add files via upload

---
+++ base de datos.sql
@@ -0,0 +1,19 @@
+Create table materias(
+    id INT,
+    nombre Varchar(30),
+    alumno Varchar(30),
+    calificacion INT
+
+)
+
+INSERT INTO materias (id,nombre,alumno,calificacion) VALUES
+(1,'matematicas','eric',9),
+(2,'matematicas','juan',8),
+(3,'matematicas','alex',9),
+(4,'matematicas','danha',6),
+(5,'matematicas','cesar',7),
+(6,'matematicas','karen',5),
+(7,'matematicas','kevin',10),
+(8,'matematicas','saul',8),
+(9,'matematicas','octavio',6),
+(10,'matematicas','antonio',7)
