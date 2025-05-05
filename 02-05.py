44c2b3ab806689f558043605779f0a6abdb959cc
erickevsh <eric.flores.2024@cbtis-246.edu.mx>
2025-05-05 00:07:39 -0600

    Add files via upload

---
+++ 02-05.py
@@ -0,0 +1,11 @@
+import pandas as pd
+df.pd.read_csv('listing.csv')
+df.head() #Primeros registros
+df.tail(n=10) #Ultimos 10 Registros
+df.describe()
+df.info(show_counts=True,memory_usage=True,Verbose=True)
+df.Shape
+df.columns
+list(df.columns)
+df[['name']]
+
